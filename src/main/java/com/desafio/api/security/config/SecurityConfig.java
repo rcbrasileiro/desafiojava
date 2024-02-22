0cb0b4f765dd8474ac12d6592f0df7998686efd8
Rodrigo Brasileiro <rcbrasileiro@gmail.com>
2024-02-22 15:48:36 -0300

    d

--- src/main/java/com/desafio/api/security/config/SecurityConfig.java
+++ src/main/java/com/desafio/api/security/config/SecurityConfig.java
@@ -41,17 +41,14 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.requiresChannel(rc -> rc.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-				.requiresSecure())
-				.csrf(csrf -> csrf.disable())
+		return http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers(PUBLIC_ACCESS).permitAll().anyRequest().authenticated())
 				.headers(headers -> headers.frameOptions(f -> f.sameOrigin()))
 				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authenticationProvider(authenticationProvider())
 				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-				.addFilterBefore(exceptionAuthFilter, JwtAuthFilter.class)
-				.build();
+				.addFilterBefore(exceptionAuthFilter, JwtAuthFilter.class).build();
 	}
 
 	@Bean
