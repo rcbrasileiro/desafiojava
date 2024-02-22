6b0071bceea57391bd23565b6b02c040465124d5
Rodrigo Brasileiro <rcbrasileiro@gmail.com>
2024-02-22 12:10:55 -0300

    Authentication filter created

--- src/main/java/com/desafio/api/security/filter/JwtAuthFilter.java
+++ src/main/java/com/desafio/api/security/filter/JwtAuthFilter.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,11 +14,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.desafio.api.security.UserDetailsServiceImpl;
 import com.desafio.api.security.service.JwtService;
-import com.desafio.api.web.exception.RestError;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.desafio.api.security.util.Access;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -32,16 +28,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 	private JwtService jwtService;
 
 	@Autowired
-	UserDetailsServiceImpl userDetailsServiceImpl;
-
-	private static final String[] IGNORED_URLS = { "/h2-console/", "/api/users", "/api/signin" };
+	private UserDetailsServiceImpl userDetailsServiceImpl;
 
 	@Override
 	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
 		String requestUri = request.getRequestURI();
-		if (Arrays.stream(IGNORED_URLS).anyMatch(requestUri::startsWith)) {
+		if (Arrays.stream(Access.IGNORED_URLS).anyMatch(requestUri::startsWith)) {
 			filterChain.doFilter(request, response);
 			return;
 		}
@@ -51,22 +45,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 		String username = null;
 		if (Objects.nonNull(authHeader) && authHeader.startsWith("Bearer ")) {
 			token = authHeader.substring(7);
-			try {
-				username = this.jwtService.extractUsername(token);
-
-			} catch (ExpiredJwtException e) {
-				response.setStatus(HttpStatus.UNAUTHORIZED.value());
-				response.getWriter().write(convertObjectToJson(
-						new RestError("Unauthorized - invalid session", HttpStatus.UNAUTHORIZED.value())));
-				return;
-			}
-		}
-
-		if (Objects.isNull(token)) {
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			response.getWriter()
-					.write(convertObjectToJson(new RestError("Unauthorized", HttpStatus.UNAUTHORIZED.value())));
-			return;
+			username = this.jwtService.extractUsername(token);
 		}
 
 		if (Objects.nonNull(username) && Objects.isNull(SecurityContextHolder.getContext().getAuthentication())) {
@@ -79,13 +58,5 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 			}
 		}
 		filterChain.doFilter(request, response);
-	}
-
-	private String convertObjectToJson(Object object) throws JsonProcessingException {
-		if (object == null) {
-			return null;
-		}
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.writeValueAsString(object);
 	}
 }
---
+++ src/main/java/com/desafio/api/security/util/Access.java
@@ -0,0 +1,7 @@
+package com.desafio.api.security.util;
+
+public class Access {
+	
+	public static final String[] IGNORED_URLS = { "/h2-console/", "/api/users", "/api/signin" };
+
+}
